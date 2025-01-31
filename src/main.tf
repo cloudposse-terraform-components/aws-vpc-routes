c56b2e11831191b9123e2004abbc1f205b13bbe4
milldr <miller0daniel@gmail.com>
2025-01-31 11:39:36 -0500

    upstream

--- src/main.tf
+++ src/main.tf
@@ -2,7 +2,19 @@ locals {
   enabled = module.this.enabled
 }
 
+resource "aws_route" "this" {
+  for_each = local.enabled ? {
+    for pair in setproduct(var.route_table_ids, var.routes) : "${pair[0]}-${pair[1].destination.cidr_block}" => {
+      route_table_id         = pair[0]
+      destination_cidr_block = pair[1].destination.cidr_block
+      target                 = pair[1].target
+    }
+  } : {}
 
+  route_table_id         = each.value.route_table_id
+  destination_cidr_block = each.value.destination_cidr_block
 
-
-
+  # Exactly one of the following options should be given
+  transit_gateway_id = each.value.target.type == "transit_gateway_id" ? each.value.target.value : null
+  nat_gateway_id     = each.value.target.type == "nat_gateway_id" ? each.value.target.value : null
+}
